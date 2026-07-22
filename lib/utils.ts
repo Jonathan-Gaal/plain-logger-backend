65f1cb8bc8132948fd22b49391676c3bcccdcc6a
Jonathan-Gaal <jonathangaal@pursuit.org>
2026-07-22 17:09:19 -0400

    removed leftover styles files

--- lib/utils.ts
+++
@@ -1,9 +0,0 @@
-// lib/utils.ts
-
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/** Combines conditional class names and resolves Tailwind class conflicts. */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
